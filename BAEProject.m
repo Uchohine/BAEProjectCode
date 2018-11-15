c33f6fc81e0bfc522ac574a5d890d794c40d67d2
Uchohine <wxm1210@vt.edu>
2018-11-15 11:34:16 -0500

    change the structure to M-ary FSK signal generate, using blackingman bandpass filter at specified center frequency

--- BAEProject.m
+++ BAEProject.m
@@ -1,7 +1,7 @@
 
 X   = load('Signal.dat');
 X   = X(:)'; 
-SNR = 0.0001;
+SNR = 0.00001;
 Fs = X(end); % Sampling frequency (samples per second) 
 X(end)=[];
 T = 1/Fs; % seconds per sample 
@@ -14,31 +14,65 @@ Y = fft(X,n,dim);
 P2 = abs(Y/L);
 P1 = P2(:,1:n/2+1);
 P1(:,2:end-1) = 2*P1(:,2:end-1);
-Xd = awgn(X, SNR);
+Xd = awgn(X, SNR, 'measured');
 Yd = fft(Xd,n,dim);
 P4 = abs(Yd/L);
 P3 = P4(:,1:n/2+1);
 P3(:,2:end-1) = 2*P3(:,2:end-1);
-Xf = bandpass(Xd, [1490 1510], Fs) * L;
+%filter start
+filter500BMCoe = load('filter500BM.dat');
+filter1500BMCoe = load('filter1500BM.dat');
+filter2500BMCoe = load('filter2500BM.dat');
+filter3500BMCoe = load('filter3500BM.dat');
+filter500BM = dfilt.dfsymfir(filter500BMCoe);
+fvtool(filter500BM);
+filter1500BM = dfilt.dfsymfir(filter1500BMCoe);
+fvtool(filter1500BM);
+filter2500BM = dfilt.dfsymfir(filter2500BMCoe);
+fvtool(filter2500BM);
+filter3500BM = dfilt.dfsymfir(filter3500BMCoe);
+fvtool(filter3500BM);
+%1500Hz bandpass filter
+Xf = filter(filter1500BM, Xd);
 Yf = fft(Xf,n,dim);
 P6 = abs(Yf/L);
 P5 = P6(:,1:n/2+1);
 P5(:,2:end-1) = 2*P5(:,2:end-1);
-Xf2 = bandpass(Xd, [1990 2010], Fs) * L;
+%500Hz bandpass filter
+Xf2 = filter(filter500BM, Xd);
 Yf2 = fft(Xf2,n,dim);
 P8 = abs(Yf2/L);
 P7 = P8(:,1:n/2+1);
 P7(:,2:end-1) = 2*P7(:,2:end-1);
+%2500Hz bandpass filter
+Xf3 = filter(filter2500BM, Xd);
+Yf3 = fft(Xf3,n,dim);
+P10 = abs(Yf3/L);
+P9 = P10(:,1:n/2+1);
+P9(:,2:end-1) = 2*P9(:,2:end-1);
+%3500Hz bandpass filter
+Xf4 = filter(filter3500BM, Xd);
+Yf4 = fft(Xf4,n,dim);
+P12 = abs(Yf4/L);
+P11 = P12(:,1:n/2+1);
+P11(:,2:end-1) = 2*P11(:,2:end-1);
+%plot 
 figure;
-subplot (8, 1, 1), plot(t(1:L),X(1:L));
-subplot (8, 1, 2), plot(0:(Fs/n):(Fs/2-Fs/n),P1(1:n/2));
+subplot (12, 1, 1), plot(t(1:L),X(1:L));
+subplot (12, 1, 2), plot(0:(Fs/n):(Fs/2-Fs/n),P1(1:n/2));
 xlabel('Original');
-subplot (8, 1, 3), plot(t(1:L),Xd(1:L));
-subplot (8, 1, 4), plot(0:(Fs/n):(Fs/2-Fs/n),P3(1:n/2));
+subplot (12, 1, 3), plot(t(1:L),Xd(1:L));
+subplot (12, 1, 4), plot(0:(Fs/n):(Fs/2-Fs/n),P3(1:n/2));
 xlabel('Distorted');
-subplot (8, 1, 5), plot(t(1:L),Xf(1:L));
-subplot (8, 1, 6), plot(0:(Fs/n):(Fs/2-Fs/n),P5(1:n/2));
-xlabel('Filtered 1500 (bit 1)');
-subplot (8, 1, 7), plot(t(1:L),Xf2(1:L));
-subplot (8, 1, 8), plot(0:(Fs/n):(Fs/2-Fs/n),P7(1:n/2));
-xlabel('Filtered 2000 (bit 0)');
+subplot (12, 1, 5), plot(t(1:L),Xf2(1:L));
+subplot (12, 1, 6), plot(0:(Fs/n):(Fs/2-Fs/n),P7(1:n/2));
+xlabel('Filtered 500 (bit 00)');
+subplot (12, 1, 7), plot(t(1:L),Xf(1:L));
+subplot (12, 1, 8), plot(0:(Fs/n):(Fs/2-Fs/n),P5(1:n/2));
+xlabel('Filtered 1500 (bit 01)');
+subplot (12, 1, 9), plot(t(1:L),Xf3(1:L));
+subplot (12, 1, 10), plot(0:(Fs/n):(Fs/2-Fs/n),P9(1:n/2));
+xlabel('Filtered 2500 (bit 10)');
+subplot (12, 1, 11), plot(t(1:L),Xf4(1:L));
+subplot (12, 1, 12), plot(0:(Fs/n):(Fs/2-Fs/n),P11(1:n/2));
+xlabel('Filtered 3500 (bit 11)');
--- main.cpp
+++ main.cpp
@@ -14,51 +14,72 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::string input = argv[1];
-    const int sampleTime = 32;
-    const double f1 = 1500;// frequency for bit 1
-    const double f2 = 2000;// frequency for bit 0
+    const int sampleTime = 256;
+    const double f1 = 500;// frequency for bit 00
+    const double f2 = 1500;// frequency for bit 01
+    const double f3 = 2500;// frequency for bit 10
+    const double f4 = 3500;// frequency for bit 11
     const double A = 1;//amplitude
     const double PI = std::atan2(0, -1);
     std::ofstream out;
     out.open("Signal.dat");
-    const double fs = 16000;
+    const double fs = 10000;
     const double T = 1/fs;
     bool proceed = true;
-    for(auto a : input)
+    while(!input.empty())
     {
+        std::string a = input.substr(0, 2);
+        input = input.substr(2, input.back());
         if(proceed)
         {
-            switch (a) {
-                case '1':
+            if(a == "00")
+            {
+                double Fstart = 0;
+                for(int start = 0; start != sampleTime; ++start)
                 {
-                    double Fstart = 0;
-                    for(int start = 0; start != sampleTime; ++start)
-                    {
-                        double x = 2 * f1 * PI * Fstart;
-                        float y = A * std::cos(x);
-                        out << y << std::endl;
-                        Fstart += T;
-                    }
+                    double x = 2 * f1 * PI * Fstart;
+                    float y = A * std::cos(x);
+                    out << y << std::endl;
+                    Fstart += T;
                 }
-                    break;
-                case '0':
+            }
+            else if(a == "01")
+            {
+                double Fstart = 0;
+                for(int start = 0; start != sampleTime; ++start)
                 {
-                    double Fstart = 0;
-                    for(int start = 0; start != sampleTime; ++start)
-                    {
-                        double x = 2 * f2 * PI * Fstart;
-                        float y =  A * std::cos(x);
-                        out << y << std::endl;
-                        Fstart += T;
-                    }
+                    double x = 2 * f2 * PI * Fstart;
+                    float y =  A * std::cos(x);
+                    out << y << std::endl;
+                    Fstart += T;
                 }
-                    break;
-                default:
+            }
+            else if(a == "10")
+            {
+                double Fstart = 0;
+                for(int start = 0; start != sampleTime; ++start)
                 {
-                    std::cout << "ERROR INPUT" << std::endl;
-                    proceed = false;
+                    double x = 2 * f3 * PI * Fstart;
+                    float y = A * std::cos(x);
+                    out << y << std::endl;
+                    Fstart += T;
                 }
-                    break;
+            }
+            else if(a == "11")
+            {
+                double Fstart = 0;
+                for(int start = 0; start != sampleTime; ++start)
+                {
+                    double x = 2 * f4 * PI * Fstart;
+                    float y = A * std::cos(x);
+                    out << y << std::endl;
+                    Fstart += T;
+                }
+            }
+            else
+            {
+                std::cout << "ERROR INPUT" << std::endl;
+                proceed = false;
             }
         }
         else
